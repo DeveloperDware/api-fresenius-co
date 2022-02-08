aa2b4ff2aeeae27ca530a57a4606ee5da1f95e75
Danny Gomez <dannystiveng2@gmail.com>
2022-02-08 13:01:37 -0500

    cambio url

--- app/Http/Resources/Documento.php
+++ app/Http/Resources/Documento.php
@@ -21,7 +21,7 @@ class Documento extends JsonResource
             "Id" => 	$this->dp_id,
             "Title" => 	$this->dp_archivo,
             "Date" => $dp_freg->format("c"),
-            "Url"=> "https://www.dwareltda.com/freseniusHC/admintranet/verArchivoPacientesAPI.php?na=$this->dp_archivo",
+            "Url"=> "http://10.223.68.2/historiasClinicas/admintranet/verArchivoPacientesAPI.php?na=$this->dp_archivo",
             "ContentType"=>"application/".$archivo[count($archivo)-1]
         ];
     }
